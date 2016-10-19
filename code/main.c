d8835e036dbff9f1508082cc5896211fa36a4ec5
DESKTOP-OO6AU4U\Le Nham <nhamlevan.92@gmail.com>
2016-10-19 16:57:41 +0700

    main

--- code/main.c
+++ code/main.c
@@ -1,7 +1,8 @@
 #include <avr/io.h>
 
 #include "lcd_nokia5110.h"
-#include "spi.h"
+//#include "spi.h"
+#include "MFRC522.h"
 //#define as PORTB.2
 
 int main(void){
@@ -17,7 +18,7 @@ int main(void){
 	lcd_putChar('X');
 	//lcd_move(1,3);
 	char s[]="nham la ai?";
-	//lcd_putString(s);
+	lcd_putString(s);
 	//lcd_move(1,1);
 	//lcd_putChar('K');
 	while(1);
