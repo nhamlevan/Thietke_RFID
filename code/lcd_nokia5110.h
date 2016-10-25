503e82497b723d1033561f944521f22ab1fcd796
DESKTOP-OO6AU4U\Le Nham <nhamlevan.92@gmail.com>
2016-10-25 17:43:05 +0700

    cap nhat thu vien nokia 5110

--- code/lcd_nokia5110.h
+++ code/lcd_nokia5110.h
@@ -1,3 +1,4 @@
+
 #include <util/delay.h>
 #include "font6x8.h"
 
@@ -10,8 +11,8 @@
 #define PORT_LCD PORTD
 #define DDR_LCD DDRD
 
-#define sbi(port,bit) port|=_BV(bit)
-#define cbi(port,bit) port&=~_BV(bit)
+//#define sbi(port,bit) port|=_BV(bit)
+//#define cbi(port,bit) port&=~_BV(bit)
 
 #define CE_HIGH PORT_LCD|=_BV(CE)
 #define RST_HIGH PORT_LCD|=_BV(RST)
@@ -26,48 +27,61 @@
 #define CLK_LOW PORT_LCD&=~_BV(CLK)
 
 #define size_X 84
-#define size_Y 48
+#define size_Y 6
 
 void lcd_Write(uint8_t data){
-	//uint8_t i;
-	//DC_LOW;
+
 	for(uint8_t i=0;i<8;i++){
 		if(data&(0x80>>i)) DIN_HIGH;
 		else DIN_LOW;
 		
 		CLK_HIGH;
-		_delay_ms(100);
 		CLK_LOW;
+		
 	}
+	_delay_us(400);
 }
 
 void lcd_cmd(uint8_t cmd){
+  	//_delay_ms(10);
 	DC_LOW;
 	lcd_Write(cmd);
 }
 
+
+//#define lcd_cmd(cmd) {DC_LOW; lcd_Write(cmd); }
+//#define lcd_gotoXY(y,x) {CE_LOW; lcd_cmd(0x40|(y-1)); lcd_cmd(0x80|(x-1)*0x06);}
 void lcd_gotoXY(uint8_t y,uint8_t x){
+	//_delay_ms(10);
 	CE_LOW;
-	lcd_cmd(0x40|y);
-	lcd_cmd(0x80|x);
+	lcd_cmd(0x40|(y-1));
+	lcd_cmd(0x80|(x-1)*0x06);
 }
 
 void lcd_putChar(uint8_t chr){
 	uint8_t b=chr-32;
-	uint8_t line;
+	//uint8_t line;
 	DC_HIGH;
-	CE_HIGH;
-	for(line=0;line<5;line++){
+
+	lcd_Write(~ASCII[b][0]);
+	lcd_Write(~ASCII[b][1]);
+	lcd_Write(~ASCII[b][2]);
+	lcd_Write(~ASCII[b][3]);
+	lcd_Write(~ASCII[b][4]);
+	lcd_Write(~ASCII[b][5]);
+	/*for(line=0;line<6;line++){
 		//CE_HIGH;
-		lcd_Write(ASCII[b][line]);
-	}
+		lcd_Write(~ASCII[b][line]);
+	}*/
 }
 
 void lcd_clear(){
 	uint8_t x,y;
+	CE_LOW;
+	DC_HIGH;
 	for(y=0;y<size_Y;y++){
 		for(x=0;x<size_X;x++){
-			lcd_putChar(0x00);
+			lcd_Write(0xff);
 		}
 	}
 }
@@ -83,20 +97,27 @@ void lcd_init(){
 	DDR_LCD=0xff;
 
 	CLK_LOW;
-	_delay_us(100);
+	_delay_ms(2);
 	RST_LOW;
-	_delay_us(100);
+	_delay_ms(2);
 	RST_HIGH;
-	_delay_us(100);
+	_delay_ms(2);
 	CE_LOW;
-	_delay_us(100);
+	_delay_ms(2);
 	
 	lcd_cmd(0x21);
-	lcd_cmd(0xB1);
 	lcd_cmd(0xC0);
-	lcd_cmd(0x04);
-	lcd_cmd(0x14);
 	lcd_cmd(0x20);
 	lcd_cmd(0x0D);
 	
+}
+
+
+void lcd_putStringSX(uint8_t *s, uint8_t size){
+	//while(*s){
+		//lcd_putChar(*s);
+		//s++;
+	//}//
+	uint8_t i;
+	for(i=0;i<size;i++) lcd_putChar(s[i]);
 }
